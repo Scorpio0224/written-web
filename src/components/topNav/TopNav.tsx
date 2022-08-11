2ccd5dda3b58f05c6ba593f5b9da85c5282b52c4
liuyang <ly2548190794@163.com>
2022-08-11 18:00:49 +0800

    feat: 增加下载组件

--- src/components/topNav/TopNav.tsx
+++ src/components/topNav/TopNav.tsx
@@ -3,9 +3,14 @@ import react from 'react';
 import writtenLogo from '../../images/written.svg';
 
 const TopNav = () => {
+    const handleClick = () => {
+        location.href = '../../assets/written-1.0.1.dmg';
+    }
+
     return (
         <div className="top-nav-box">
             <img src={writtenLogo} alt="logo" />
+            <a download href="../../public/dashboard-bgc-color.svg">Download</a>
         </div>
     )
 };
