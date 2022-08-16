07678f185a9ab38dda67d78ad2edcbc8218ef3b8
liuyang <ly2548190794@163.com>
2022-08-16 17:27:51 +0800

    feat: 鼠标样式调整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,28 @@ import Dashboard from '@/components/Dashboard/Dashboard';
 import './index.global.less';
 import '../i18n/index';
 import { useEffect, useState } from 'react';
+import { useMouse } from 'ahooks';
+import favico from '../images/written.svg';
 
 export default function HomePage() {
   const [dashboardShow, setBoardShow] = useState(false);
 
+  const state: {
+    pageX: number,
+    pageY: number,
+  } = useMouse();
+
+  const visiable = () => {
+    const isShow = (state.pageX > 0 || state.pageY > 0) && state.pageX && state.pageY;
+
+    return isShow ? 1 : 0;
+  }
+
   return (
     <div>
+      <div className='mouse-box' style={{top: `${state.pageY}px`, left: `${state.pageX}px`, opacity: visiable()}}>
+        <img src={favico} alt="" />
+      </div>
       <TopNav />
       <div className='content-box'>
         <ContentTitle />
